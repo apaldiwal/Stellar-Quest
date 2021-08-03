574382fb0e0698474703323ad4f33d5beaa83352
Abhimanyu Paldiwal <paldiwal.abhimanyu@protonmail.com>
2021-08-03 14:20:26 -0700

    Merge claimable-balance-part-*.py

--- bounty-program/stellar-docs/python/claimable-balance.py
+++ bounty-program/stellar-docs/python/claimable-balance.py
@@ -61,3 +61,52 @@ try:
     print("Claimable balance created!")
 except (BadRequestError, BadResponseError) as err:
     print(f"Tx submission failed: {err}")
+
+# Method 1: Not available in the Python SDK yet.
+
+# Method 2: Suppose `txResponse` comes from the transaction submission
+# above.
+txResult = TransactionResult.from_xdr(txResponse["result_xdr"])
+results = txResult.result.results
+
+# We look at the first result since our first (and only) operation
+# in the transaction was the CreateClaimableBalanceOp.
+operationResult = results[0].tr.create_claimable_balance_result
+balanceId = operationResult.balance_id.to_xdr_bytes().hex()
+print(f"Balance ID (2): {balanceId}")
+
+# Method 3: Account B could alternatively do something like:
+try:
+    balances = (
+        server
+        .claimable_balances()
+        .for_claimant(B.public_key)
+        .limit(1)
+        .order(desc = True)
+        .call()
+    )
+except (BadRequestError, BadResponseError) as err:
+    print(f"Claimable balance retrieval failed: {err}")
+
+balanceId = balances["_embedded"]["records"][0]["id"]
+print(f"Balance ID (3): {balanceId}")
+
+claimBalance = ClaimClaimableBalance(balance_id = balanceId)
+print(f"{A.public_key} claiming {balanceId}")
+
+tx = (
+    TransactionBuilder (
+        source_account = aAccount,
+        network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE,
+        base_fee = server.fetch_base_fee()
+    )
+    .append_operation(claimBalance)
+    .set_timeout(180)
+    .build()
+)
+
+tx.sign(A)
+try:
+    txResponse = server.submit_transaction(tx)
+except (BadRequestError, BadResponseError) as err:
+    print(f"Tx submission failed: {err}")
