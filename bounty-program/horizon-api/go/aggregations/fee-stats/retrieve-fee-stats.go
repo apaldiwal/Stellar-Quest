c10d18cfb6cf0c979fbead8e9c9c681d1955e8eb
Abhimanyu Paldiwal <paldiwal.abhimanyu@protonmail.com>
2021-08-07 15:07:39 -0700

    Create retrieve-fee-stats.go

---
+++ bounty-program/horizon-api/go/aggregations/fee-stats/retrieve-fee-stats.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+  "fmt"
+  "log"
+
+  "github.com/stellar/go/clients/horizonclient"
+)
+
+func main() {
+  client := horizonclient.DefaultPublicNetClient
+
+  resp, err := client.FeeStats()
+  if err != nil {
+    log.Fatal(err)
+    return
+  }
+  fmt.Println(resp)
+}
